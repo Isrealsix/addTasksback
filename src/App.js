80f63f8fc7db5e52621f8312672a0484cf0647a0
Isreal6ix <isrealsix@gmail.com>
2021-07-30 20:08:01 +0700

    added method to POST and GET

--- src/App.js
+++ src/App.js
@@ -89,21 +89,33 @@ function App() {
 			const response = await fetch(
 				'https://appseven-9144b-default-rtdb.firebaseio.com/movies.json'
 			);
+
 			if (!response.ok) {
 				throw new Error('Something went wrong!');
 			}
 
 			const data = await response.json();
+			const loadedMovies = [];
 
-			const transformedMovies = data.results.map(movieData => {
-				return {
-					id: movieData.episode_id,
-					title: movieData.title,
-					openingText: movieData.opening_crawl,
-					releaseDate: movieData.release_date,
-				};
-			});
-			setMovies(transformedMovies);
+			for (const key in data) {
+				loadedMovies.push({
+					id: key,
+					title: data[key].title,
+					openingText: data[key].openingText,
+					releaseDate: data[key].releaseDate,
+				});
+			}
+
+			// const transformedMovies = data.results.map(movieData => {
+			// 	return {
+			// 		id: movieData.episode_id,
+			// 		title: movieData.title,
+			// 		openingText: movieData.opening_crawl,
+			// 		releaseDate: movieData.release_date,
+			// 	};
+			// });
+
+			setMovies(loadedMovies);
 		} catch (error) {
 			setError(error.message);
 		}
@@ -114,8 +126,20 @@ function App() {
 		fetchMoviesHandler();
 	}, [fetchMoviesHandler]);
 
-	function addMovieHandler(movie) {
-		console.log(movie);
+	async function addMovieHandler(movie) {
+		const response = await fetch(
+			'https://appseven-9144b-default-rtdb.firebaseio.com/movies.json',
+			{
+				method: 'POST',
+				body: JSON.stringify(movie),
+				headers: {
+					'Content-Type': 'application/json',
+				},
+			}
+		);
+
+		const data = await response.json();
+		console.log(data);
 	}
 
 	let content = <p>Found no movies.</p>;
