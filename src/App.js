69e159a90d031cf4c08e3512b65f876be7629d8e
Isreal6ix <isrealsix@gmail.com>
2021-07-30 19:10:21 +0700

    used useEffect for requests and useCallback for saving the async function

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 
 import MoviesList from './components/MoviesList';
 import './App.css';
@@ -8,10 +8,11 @@ function App() {
 	const [isLoading, setIsLoading] = useState(false);
 	const [error, setError] = useState(null);
 
-	async function fetchMoviesHandler() {
+	const fetchMoviesHandler = useCallback(async () => {
 		setIsLoading(true);
+		setError(null);
 		try {
-			const response = await fetch('https://swapi.dev/api/film');
+			const response = await fetch('https://swapi.dev/api/films');
 			if (!response.ok) {
 				throw new Error('Somethin went wrong (;');
 			}
@@ -28,8 +29,13 @@ function App() {
 			setIsLoading(false);
 		} catch (error) {
 			setError(error.message);
+			setIsLoading(false);
 		}
-	}
+	}, []);
+
+	useEffect(() => {
+		fetchMoviesHandler();
+	}, [fetchMoviesHandler]);
 
 	let content;
 
